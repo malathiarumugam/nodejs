d79e14668ea9eefbb92076e85260b898bf48f099
Malathi <malathim993@gmail.com>
2016-02-24 17:24:47 +0530

    Update webservice.js

--- webservice.js
+++ webservice.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var port = 8082;
+var host = "0.0.0.0";
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   var d = new Date();
   
-  res.end('Hello World\n' +d.toString());
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+  res.end('Hello World\n This is Sample nodejs Application' +d.toString());
+}).listen(port,host);
+console.log('Server running at http://0.0.0.0:8082/');
+
